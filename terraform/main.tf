b4f1d043ba1121e6495bf56b5d2282028c619761
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2026-01-01 16:40:55 +0000

    fix: inject GSA email via valuesFrom to satisfy Workload Identity

--- terraform/main.tf
+++ terraform/main.tf
@@ -285,6 +285,7 @@ resource "kubernetes_config_map" "flux_vars" {
   }
 
   data = {
-    GCP_PROJECT_ID = var.gcp_project_id
+    GCP_PROJECT_ID            = var.gcp_project_id
+    GCP_SERVICE_ACCOUNT_EMAIL = google_service_account.gke_nodes.email
   }
 }
