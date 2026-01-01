4b51f671ddc1cc6d3a549f54bba808fbc06a7339
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2026-01-01 18:06:22 +0000

    style: terraform fmt

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -1,7 +1,7 @@
 resource "flux_bootstrap_git" "this" {
   depends_on = [google_container_cluster.primary]
 
-  embedded_manifests     = true
-  path                   = "flux/clusters/production"
-  components_extra       = ["image-reflector-controller", "image-automation-controller"]
+  embedded_manifests = true
+  path               = "flux/clusters/production"
+  components_extra   = ["image-reflector-controller", "image-automation-controller"]
 }
